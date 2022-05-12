cf7fada70cab17dd638a0f88e77f14614e75bfd6
Mythbusters123 <atr10605@hotmail.com>
2022-05-12 19:14:24 -0400

    Dark mode, accessory items

--- rebuild_site.sh
+++ rebuild_site.sh
@@ -2,7 +2,5 @@
 echo './blogit clean build'
 ./blogit clean build
 
-cat << __EOF__
-find data -type f -exec cp {} blog/ \;
-__EOF__
+echo 'find data -type f -exec cp {} blog/ \;'
 find data -type f -exec cp {} blog/ \;
